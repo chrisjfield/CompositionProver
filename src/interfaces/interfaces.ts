154fd841169579ddf1de2e474bc2a96c4cda9f5f
chris <chris.j.field1992@gmail.com>
2019-07-08 08:01:36 +0100

    committing file for name case change

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,108 @@
+import { EDIT_CALL, EDIT_METHOD, ADD_METHOD, DELETE_METHOD, EDIT_SETTINGS, EDIT_CURRENT_COMPOSITION, DELETE_COMPOSITION, ADD_COMPOSITION, EDIT_COMPOSITION } from "../redux/actions/actionTypes";
+import { ICurrentCompositionState } from "../redux/reducers/currentCompositionReducer";
+
+export interface ICall {
+    name: string;
+    abbreviation: string;
+    stage: number;
+    leadEndPlaceNotation?: string;
+    halfLeadPlaceNotation?: string;
+    editable: boolean;
+}
+
+export interface IStage {
+    name: string;
+    stage: number;
+}
+
+export interface IMethod {
+    name: string;
+    abbreviation: string;
+    placeNotation: string;
+    stage: number;
+    editable: boolean;
+    defaultBob?: string;
+    defaultSingle?: string;
+}
+
+export interface IComposition {
+    numberOfBells: number;
+    style: 'full' | 'numeric' | 'positional';
+    parts: number;
+    composition?: string;
+}
+
+export interface IEditCallAction {
+    type: typeof EDIT_CALL;
+    payload: ICall;
+}
+
+export type ICallActionTypes = IEditCallAction
+
+interface IEditMethodAction {
+    type: typeof EDIT_METHOD;
+    payload: IMethod[];
+}
+
+interface IAddMethodAction {
+    type: typeof ADD_METHOD;
+    payload: IMethod;
+}
+
+interface IDeleteMethodAction {
+    type: typeof DELETE_METHOD;
+    payload: string;
+}
+
+export type IMethodActionTypes = IEditMethodAction | IAddMethodAction | IDeleteMethodAction
+
+
+interface ICompositionAction {
+    type: typeof EDIT_CURRENT_COMPOSITION;
+    payload: IComposition;
+}
+
+export type ICompositionActionTypes = ICompositionAction
+
+export interface ISettings {
+    methodStage: number;
+}
+
+export interface IEditSettingsAction {
+    type: typeof EDIT_SETTINGS;
+    payload: ISettings;
+}
+
+export type ISettingsActionTypes = IEditSettingsAction
+
+export interface ISavedComposition {
+    name: string;
+    compositionState: ICurrentCompositionState;
+}
+
+interface IDeleteSavedCompositionAction {
+    type: typeof DELETE_COMPOSITION;
+    payload: string;
+}
+
+interface IAddSavedCompositionAction {
+    type: typeof ADD_COMPOSITION;
+    payload: ISavedComposition;
+}
+
+interface IEditSavedCompositionAction {
+    type: typeof EDIT_COMPOSITION;
+    payload: ISavedComposition[];
+}
+
+export type ISavedCompositionActionTypes = IDeleteSavedCompositionAction | IAddSavedCompositionAction | IEditSavedCompositionAction
+
+export interface IStageSelectorState {
+    stage: number;
+    setStage(stage: number): void;
+}
+
+export interface ICallState {
+    calls: ICall[];
+    editCall(call: ICall): void;
+}
