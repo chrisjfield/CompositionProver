052ae86be2ea2259e0ac12048c2ed8d3b32190d2
chris field <chris.field@theaccessgroup.com>
2019-07-16 14:47:08 +0100

    starting to look at linting.

--- src/redux/reducers/currentCompositon/callReducer.ts
+++ src/redux/reducers/currentCompositon/callReducer.ts
@@ -3,13 +3,13 @@ import { EDIT_CALL } from "../../actions/actionTypes";
 import defaultCalls from "../../../defaults/calls";
 
 export default function (state: ICall[] = defaultCalls, action: ICallActionTypes) {
-  switch (action.type) {
-    case EDIT_CALL: {
-      return [...state.map((call) => {
-        return call.abbreviation === action.payload.abbreviation && call.stage === action.payload.stage ? action.payload : call;
-      })]
+    switch (action.type) {
+        case EDIT_CALL: {
+            return [...state.map((call) => {
+                return call.abbreviation === action.payload.abbreviation && call.stage === action.payload.stage ? action.payload : call;
+            })];
+        }
+            default:
+        return state;
     }
-    default:
-      return state;
-  }
 }
--- src/redux/reducers/currentCompositon/methodReducer.ts
+++ src/redux/reducers/currentCompositon/methodReducer.ts
@@ -1,21 +1,32 @@
-import { IMethod, IMethodActionTypes } from "../../../interfaces/interfaces";
-import { DELETE_METHOD, EDIT_METHOD, ADD_METHOD } from "../../actions/actionTypes";
-import defaultMethods from "../../../defaults/methods";
+import { IMethod, IMethodActionTypes } from '../../../interfaces/interfaces';
+import {
+	DELETE_METHOD,
+	EDIT_METHOD,
+	ADD_METHOD,
+} from '../../actions/actionTypes';
+import defaultMethods from '../../../defaults/methods';
 
-export default function (state: IMethod[] = defaultMethods, action: IMethodActionTypes) {
-  switch (action.type) {
-    case ADD_METHOD: {
-        return [...state, {...action.payload, id: state.length}];
-    }
-    case EDIT_METHOD: {
-        return [...state.map((method) => {
-            return method.id === action.payload.id ? action.payload : method;
-          })]
-    }
-    case DELETE_METHOD: {
-        return [...state.filter((method) => method.id !== action.payload)];
-    }
-    default:
-        return state;
-  }
+export default function(
+	state: IMethod[] = defaultMethods,
+	action: IMethodActionTypes,
+) {
+	switch (action.type) {
+		case ADD_METHOD: {
+			return [...state, { ...action.payload, id: state.length }];
+		}
+		case EDIT_METHOD: {
+			return [
+				...state.map(method => {
+					return method.id === action.payload.id
+						? action.payload
+						: method;
+				}),
+			];
+		}
+		case DELETE_METHOD: {
+			return [...state.filter(method => method.id !== action.payload)];
+		}
+		default:
+			return state;
+	}
 }
