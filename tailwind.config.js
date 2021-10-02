e50029b9407574c8092a637fc280bfbbb1e997a8
Spellcro <66977337+Spellcro@users.noreply.github.com>
2021-10-02 13:10:00 +0100

    update tailwind darkmode to false for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
