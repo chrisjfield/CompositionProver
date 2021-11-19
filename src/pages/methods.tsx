e799aded5cb237a373fe4134eb07802ccb494e6e
Spellcro <66977337+Spellcro@users.noreply.github.com>
2021-11-19 19:47:37 +0000

    Show method data in a table

--- src/pages/methods.tsx
+++ src/pages/methods.tsx
@@ -1,29 +1,44 @@
 import { useContext } from 'react';
-import { Divider, Grid } from '@mui/material';
 import MethodContext from '../context/methodContext';
 import SettingsContext from '../context/settingsContext';
 import StageSelector from '../components/stageSelector/stageSelector';
-import EditableMethod from '../components/methods/editableMethod';
-import MethodButtons from '../components/methods/methodButtons';
+import CallContext from '../context/callContext';
 
 const MethodsPage = () => {
   const { methods } = useContext(MethodContext);
   const { settings: { methodStage } } = useContext(SettingsContext);
+  const { calls } = useContext(CallContext);
+
+  const getCallName = (value: string) => calls
+    .find((call) => call.stage === methodStage && call.abbreviation === value)!.name;
+
+  const gridColumns = ['Method Name', 'Abbreviation', 'Place Notation', 'Default Bob', 'Default Single'];
 
   return (
     <>
       <StageSelector />
-      <Grid container justifyContent="space-evenly" spacing={3}>
-        {methods
-          .filter((m) => m.stage === methodStage)
-          .map((m) => (
-            <Grid item xs={12} key={`method_${m.id}`}>
-              <EditableMethod method={m} />
-              <Divider sx={{ m: 'auto' }} />
-            </Grid>
-          ))}
-      </Grid>
-      <MethodButtons />
+      <div className="max-w-7xl w-full mx-auto rounded-lg overflow-hidden border">
+        <table className="w-full">
+          <thead>
+            <tr className="overflow-hidden divide-x bg-gray-100 border-b-2">
+              {gridColumns.map((column) => <th key={column} className="text-left pl-2 pr-6 font-normal border-gray-700 text-gray-600">{column}</th>)}
+            </tr>
+          </thead>
+          <tbody className="divide-y">
+            {methods
+              .filter((method) => method.stage === methodStage)
+              .map((method) => (
+                <tr key={`method_${method.id}`} className="last:rounded-b-lg overflow-hidden even:bg-gray-50">
+                  <td>{method.name}</td>
+                  <td>{method.abbreviation}</td>
+                  <td>{method.placeNotation}</td>
+                  <td>{getCallName(method.defaultBob)}</td>
+                  <td>{getCallName(method.defaultSingle)}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
