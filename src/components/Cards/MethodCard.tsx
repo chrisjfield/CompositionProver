3e5e997c7f871e49af3e8325a5c45931b170ab85
Spellcro <66977337+Spellcro@users.noreply.github.com>
2021-11-20 11:18:06 +0000

    Add styling to focus state for edit/delete method buttons, add more info to screenreader, hide duplicate info from screenreader

--- src/components/Cards/MethodCard.tsx
+++ src/components/Cards/MethodCard.tsx
@@ -19,15 +19,15 @@ const MethodCard = ({ method }: { method: Method }) => {
       <p className="text-xl font-bold text-gray-800">{getCallName(method.defaultBob)}</p>
       <p className="text-xl font-bold text-gray-800">{getCallName(method.defaultSingle)}</p>
       <div className="flex border-t divide-x">
-        <button type="button" className="w-1/2 flex justify-center items-center py-2 rounded-bl-lg text-gray-600 hover:bg-blue-700 hover:text-white method-card--button">
-          <span className="method-card--button-label">Edit</span>
-          <span className="sr-only">Edit Method</span>
-          <PencilIcon className="w-8 h-8 fill-current" />
+        <button type="button" className="w-1/2 flex justify-center items-center py-2 rounded-bl-lg text-gray-600 hover:bg-blue-700 focus:bg-blue-700 hover:text-white focus:text-white method-card--button">
+          <span className="method-card--button-label" aria-hidden="true">Edit</span>
+          <span className="sr-only">{`Edit Method: ${method.name}`}</span>
+          <PencilIcon className="w-7 h-7 fill-current" />
         </button>
-        <button type="button" className="w-1/2 flex justify-center items-center py-2 rounded-br-lg text-gray-600 hover:bg-red-700 hover:text-white method-card--button">
-          <span className="method-card--button-label">Delete</span>
-          <span className="sr-only">Delete Method</span>
-          <BinIcon className="w-9 h-9 fill-current" />
+        <button type="button" className="w-1/2 flex justify-center items-center py-2 rounded-br-lg text-gray-600 hover:bg-red-700 focus:bg-red-700 hover:text-white focus:text-white method-card--button">
+          <span className="method-card--button-label" aria-hidden="true">Delete</span>
+          <span className="sr-only">{`Delete Method: ${method.name}`}</span>
+          <BinIcon className="w-8 h-8 fill-current" />
         </button>
       </div>
     </div>
--- src/pages/methods.tsx
+++ src/pages/methods.tsx
@@ -18,8 +18,8 @@ const MethodsPage = () => {
   return (
     <>
       <StageSelector />
-      <div className="mx-auto rounded-lg overflow-hidden border md:mx-6 lg:mx-12 xl:mx-20">
-        {/* <table className="w-full">
+      {/* <div className="mx-auto rounded-lg overflow-hidden border md:mx-6 lg:mx-12 xl:mx-20">
+        <table className="w-full">
           <thead>
             <tr className="overflow-hidden bg-gray-100 border-b-2">
               {gridColumns.map((column) => (<th key={column} className="text-left pl-2 pr-6 py-2 font-normal border-gray-700 text-gray-600">{column}</th>))}
@@ -38,8 +38,8 @@ const MethodsPage = () => {
                 </tr>
               ))}
           </tbody>
-        </table> */}
-      </div>
+        </table>
+      </div> */}
       <div className="flex flex-row flex-wrap">
         {methods
           .filter((method) => method.stage === methodStage)
